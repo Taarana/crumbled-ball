15cdc1bc3b8535d30a3b8e7b900a21b50b43b170
Taarana <81751883+Taarana@users.noreply.github.com>
2021-06-24 15:19:50 +0530

    Add files via upload

---
+++ PaperClass.js
@@ -0,0 +1,34 @@
+class PaperClass
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
